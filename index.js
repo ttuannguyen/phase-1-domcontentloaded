38f2d4b3ce062476fc9edb44aab1a6989b96e18a
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-01-07 02:30:54 -0500

    commit tree e459e89f6d0e4755b5cfe486a50e5dec651e001e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('The DOM has loaded'); //This will not be logged immediately 
+// })
+
+// console.log('"This console.log() fires when index.js loads - before DOMContentLoaded is triggered');
+//The above is logged first.
+
+
+//Use the event's callback function to target the paragraph element with id="text" 
+//and replace the text with "This is really cool!"
+document.addEventListener('DOMContentLoaded', () => {
+    const element = document.getElementById('text');
+    element.innerHTML = "<p>This is really cool!</p>";
+})
